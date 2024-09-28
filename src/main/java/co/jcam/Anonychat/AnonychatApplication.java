b3503f40617780277d44b85b79d7710001bfccc0
Alimanas <jacostam4@ucentral.edu.co>
2024-09-27 20:15:13 -0500

    first commit

---
+++ src/main/java/co/jcam/Anonychat/AnonychatApplication.java
@@ -0,0 +1,13 @@
+package co.jcam.Anonychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonychatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonychatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/jcam/Anonychat/AnonychatApplicationTests.java
@@ -0,0 +1,13 @@
+package co.jcam.Anonychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonychatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
